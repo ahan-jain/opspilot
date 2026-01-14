1f642d58b31602df806157aac9ee4d9eb42b04c3
jain-aha <ahan@ahanjain.com>
2026-01-13 22:50:00 -0500

    added index import

--- agent_service/models.py
+++ agent_service/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey, Enum as SQLEnum
+from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey, Index, Enum as SQLEnum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from datetime import datetime
